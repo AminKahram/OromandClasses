3890c0967c5a01bf26fecd2ec7fb210fc93235dd
Amin <KahramAmin@gmail.com>
2023-09-05 12:22:46 +0330

    Hidden Methos

---
+++ OOPInCSharp/HiddenMethod/Parent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInCSharp.HiddenMethod
+{
+    public class Parent
+    {
+        public string HiddenMethod() 
+            => nameof(HiddenMethod);
+    }
+    public class FirstChild:Parent
+    {
+        public new string HiddenMethod()
+            => nameof(HiddenMethod);
+    }
+}
