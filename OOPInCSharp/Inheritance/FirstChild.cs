705c38e2ca556be99664c46bdf54e534a598e55c
Amin <KahramAmin@gmail.com>
2023-09-05 11:16:24 +0330

    Basics Of Inheritance

---
+++ OOPInCSharp/Inheritance/FirstChild.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInCSharp.Inheritance
+{
+    public class FirstChild:Parent
+    {
+        public string GetChildClassName()
+            => nameof(FirstChild);
+    }
+}
---
+++ OOPInCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPInCSharp.Inheritance;
+
+Console.WriteLine("Hello, World!");
+
+Parent parent = new();
+parent.GetHashCode();
+Console.WriteLine(parent.GetClassName());
+
+FirstChild firstChild = new();
+
+Console.WriteLine(firstChild.GetClassName());
+Console.WriteLine(firstChild.GetChildClassName());
