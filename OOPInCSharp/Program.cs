c07ece87e0b5ef5dc8f58673a62a31d01cfa1d89
Amin <KahramAmin@gmail.com>
2023-09-05 12:18:58 +0330

    Virtal and Overriding

--- OOPInCSharp/Program.cs
+++ OOPInCSharp/Program.cs
@@ -1,13 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using OOPInCSharp.Inheritance;
+using OOPInCSharp.VirtualMethods;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-Parent parent = new();
-parent.GetHashCode();
-Console.WriteLine(parent.GetClassName());
+//Parent parent = new();
+//parent.GetHashCode();
+//Console.WriteLine(parent.GetClassName());
 
-FirstChild firstChild = new();
+//FirstChild firstChild = new();
 
-Console.WriteLine(firstChild.GetClassName());
-Console.WriteLine(firstChild.GetChildClassName());
+//Console.WriteLine(firstChild.GetClassName());
+//Console.WriteLine(firstChild.GetChildClassName());
+
+Animal animal = new();
+Console.WriteLine($"Animal Voice Is :");
+animal.Voice();
+
+
+Dog dog = new();
+Console.WriteLine($"Animal Voice Is :");
+dog.Voice();
+
+
+Cat cat = new();
+Console.WriteLine($"Animal Voice Is :");
+cat.Voice();
+
+
+Cow cow = new();
+Console.WriteLine($"Animal Voice Is :");
+cow.Voice();
---
+++ OOPInCSharp/VirtualMethods/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInCSharp.VirtualMethods
+{
+    public class Animal
+    {
+        public virtual void Voice()
+        {
+            Console.WriteLine("My animal voice");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void Voice()
+        {
+            base.Voice();
+            Console.WriteLine("Meow");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Voice()
+        {
+            Console.WriteLine("Hop");
+            base.Voice();
+
+        }
+    }
+
+    public class Cow : Animal
+    {
+        public override void Voice()
+        {
+  
+        }
+    }
+}
