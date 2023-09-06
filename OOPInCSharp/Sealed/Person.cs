188c5d66ce628f89e865098dc1227eccd0fd91dd
Amin <KahramAmin@gmail.com>
2023-09-06 07:25:32 +0330

    sealed classes and methos

---
+++ OOPInCSharp/Sealed/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInCSharp.Sealed
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Abbas";
+        public string LastName { get; set; } = "Abbasi";
+            
+    }
+    public sealed class Child : Person 
+    {
+
+    }
+
+}
