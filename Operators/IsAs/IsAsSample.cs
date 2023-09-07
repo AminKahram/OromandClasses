8a29b38ebeab9b135a7598ab45d81a9300daafee
Amin <KahramAmin@gmail.com>
2023-09-07 09:48:45 +0330

    Is As

---
+++ Operators/IsAs/IsAsSample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators.IsAs
+{
+    public class IsAsSample
+    {
+        public void CheckIs(object input)
+        {
+            Console.WriteLine(input is Person);
+        }
+        public void CheckAs(object input)
+        {
+            Console.WriteLine(input as Person != null ? "Is Person":"Is Not Person");
+        }
+
+    }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Student : Person
+    {
+        public string Text { get; set; }
+    }
+
+    public class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+}
--- Operators/Program.cs
+++ Operators/Program.cs
@@ -1,11 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Operators.CheckedAndUnchecked;
+using Operators.IsAs;
 
 Console.WriteLine("Hello, World!");
 
 
-CheckedAunUncheckedSample sample = new();
-sample.Checked();
+//CheckedAunUncheckedSample sample = new();
+//sample.Checked();
 //sample.Checked2();
+IsAsSample sample = new();
+var student = new Student
+{
+    FirstName = "Abbas",
+    LastName = "Anghezi",
+    Text = "efmnekjnfkebfkebf"
+};
+var teacher = new Teacher
+{
+    FirstName = "Alireaz",
+    LastName = "Oromand"
+};
+
+sample.CheckIs(student);
+sample.CheckIs(teacher);
+
+sample.CheckAs(student);
+sample.CheckAs(teacher);
 
 Console.ReadLine(); 
