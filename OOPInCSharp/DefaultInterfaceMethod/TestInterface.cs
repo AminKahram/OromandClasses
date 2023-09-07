afc0ec9b43132a50af86da43d9191e513bd42e48
Amin <KahramAmin@gmail.com>
2023-09-07 08:31:21 +0330

    Default Interface Method

---
+++ OOPInCSharp/DefaultInterfaceMethod/TestInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInCSharp.DefaultInterfaceMethod
+{
+    public interface TestInterface
+    {
+        void Log(string message);
+        void info(string message) 
+            => throw new NotImplementedException();
+    }
+    public class ImplementTestInterface : TestInterface
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void info(string message) { Console.WriteLine(); }
+    }
+}
