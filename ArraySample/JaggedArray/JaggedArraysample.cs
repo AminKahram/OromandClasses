e127656087cb7a9aa2f5d8852b0aa5dc541babb8
Amin <KahramAmin@gmail.com>
2023-09-13 15:09:24 +0330

    Jagged Array

---
+++ ArraySample/JaggedArray/JaggedArraysample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySample.JaggedArray
+{
+    public class JaggedArraysample
+    {
+        public void JaggedArray()
+        {
+            int[][] jagged = new int[2][];
+            jagged[0] = new int[] {1,2,3 };
+            jagged[1] = new int[] {1,2,3 ,4,5,66,98};
+
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.WriteLine($"jagged of{i} and {j} is: {jagged[i][j]}");
+                }
+            }
+        }
+    }
+}
--- ArraySample/Program.cs
+++ ArraySample/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+using ArraySample.JaggedArray;
 using ArraySample.MultiDimentioanlArray;
 
-TwoDimentional twoDimentional = new();
+//TwoDimentional twoDimentional = new();
 
-twoDimentional.TwoD();
+//twoDimentional.TwoD();
+
+JaggedArraysample jagged = new();
+jagged.JaggedArray();
 
 Console.ReadLine();
