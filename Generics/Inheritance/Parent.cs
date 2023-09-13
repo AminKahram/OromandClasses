23b3aeefa3bfabe9ba8b17e50983515593d656cb
Amin <KahramAmin@gmail.com>
2023-09-13 13:50:27 +0330

    Inheritance

---
+++ Generics/Inheritance/Parent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Inheritance
+{
+    public class Parent<TInput01,TInput02>
+    {
+    }
+    public class Child01:Parent<int,string>
+    {
+
+    }
+    public class Child02<TInput02> : Parent<int, TInput02>
+    {
+
+    }
+    public class Child02<TInput01, TInput02> : Parent<TInput01, TInput02>
+    {
+
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Generics.Constraints;
 using Generics.GenericClass;
+using Generics.StaticMembers;
 
 //Console.WriteLine("Hello, World!");
 
@@ -21,28 +22,40 @@ using Generics.GenericClass;
 //var result2 = genericConcat2.Concat(new Person {FirstName = "Abbas", LastName = "Abbasi"}, new Person {FirstName = "Gholam", LastName = "Gholami"});
 //Console.WriteLine(result2);
 
-Teacher teacher = new Teacher 
-{
-    FirstName = "Abbas",
-    LastName = "Abbasi"
-    ,TeacherNumber = "123"
-};
+//Teacher teacher = new Teacher 
+//{
+//    FirstName = "Abbas",
+//    LastName = "Abbasi"
+//    ,TeacherNumber = "123"
+//};
 
-Student student = new Student
-{
-    FirstName = "Asghar",
-    LastName = "asghari"
-    ,
-    StudentNumber = "1235"
-};
+//Student student = new Student
+//{
+//    FirstName = "Asghar",
+//    LastName = "asghari"
+//    ,
+//    StudentNumber = "1235"
+//};
 
-var teacherprinter = new PersonPrinter<Teacher>();
-teacherprinter.Print(teacher);
+//var teacherprinter = new PersonPrinter<Teacher>();
+//teacherprinter.Print(teacher);
 
-var studentPrinter = new PersonPrinter<Student>();
-studentPrinter.Print(student);
+//var studentPrinter = new PersonPrinter<Student>();
+//studentPrinter.Print(student);
 
-var carPrinter = new PersonPrinter<Car>();
-carPrinter.Print(new Car { Id = 2});
+//var carPrinter = new PersonPrinter<Car>();
+//carPrinter.Print(new Car { Id = 2});
+
+
+StaticParam<int>.Counter = 12;
+StaticParam<string>.Counter = 15;
+StaticParam<bool>.Counter = 1;
+
+Console.WriteLine(StaticParam<int>.Counter);
+Console.WriteLine(StaticParam<string>.Counter);
+Console.WriteLine(StaticParam<bool>.Counter);
+
+StaticParam<int>.Counter = 13;
+Console.WriteLine(StaticParam<int>.Counter);
 
 Console.ReadLine();
