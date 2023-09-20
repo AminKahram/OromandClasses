822fc08921c4e86228cd6205e68b4719d2599904
Amin <KahramAmin@gmail.com>
2023-09-20 07:38:46 +0330

    Linked List

---
+++ CollectionSample/LinkedListSamples/LinkedListSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionSample.LinkedListSamples
+{
+    public class LinkedListSample
+    {
+        LinkedList<string> list = new LinkedList<string>();
+        public void AddLast (string s)
+        {
+            list.AddLast(s);
+        }
+        public void AddSecondNode(string s)
+        {
+            LinkedListNode<string> node = new LinkedListNode<string>(s);
+            var first = list.First;
+            list.AddAfter(first, node);
+        }
+    }
+}
