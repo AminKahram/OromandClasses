8e67b6d00937136cb73b09bed7f192ac8a6bb335
Amin <KahramAmin@gmail.com>
2023-09-20 10:02:32 +0330

    Try Catch

---
+++ ExceptionHandeling/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExceptionHandeling.TryCatchSamples;
+
+Console.WriteLine("Hello, World!");
+
+Calculator.Start();
---
+++ ExceptionHandeling/TryCatchSamples/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandeling.TryCatchSamples
+{
+    public class Calculator
+    {
+        public static void Start()
+        {
+            try
+            {
+                Calculator calculator = new Calculator();
+
+                calculator.Add(0, 3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                //throw;
+            }
+
+        }
+        public void Add(int number1, int number2) 
+        {
+            if (number1 < 1)
+                throw new ArgumentException();
+            if (number2 < 1)
+                throw new InvalidOperationException();
+
+            int result = number1 + number2;
+            Console.WriteLine(result);
+
+        }
+    }
+}
