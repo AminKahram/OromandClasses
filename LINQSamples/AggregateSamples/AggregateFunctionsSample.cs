fee53fbfd8caf13dbbf6db30515a4b4d31df4d50
Amin <KahramAmin@gmail.com>
2023-09-25 07:15:47 +0330

    Aggregate and generators

---
+++ LINQSamples/AggregateSamples/AggregateFunctionsSample.cs
@@ -0,0 +1,25 @@
+﻿using LINQSampels.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSampels.AggregateSamples
+{
+    public class AggregateFunctionsSample
+    {
+        public void AggregateFunction()
+        {
+            var students = Student.GetStudents();
+
+            var totalCount = students.Count();
+            var minValue = students.Min(s => s.Grade);
+            var maxValue = students.Max(s => s.Grade);
+            var avg = students.Average(s => s.Grade);
+            var sum = students.Sum(s => s.Grade);
+
+            Console.WriteLine($"Total count : {totalCount} Min Grade : {minValue} Max Value : {maxValue} average : {avg}  Sum : {sum}");
+        }
+    }
+}
---
+++ LINQSamples/GeneratorSamples/GeneratorSample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSampels.GeneratorSamples
+{
+    public class GeneratorSample
+    {
+        public void Generators()
+        {
+            var numbers = Enumerable.Range(0, 100).ToList();
+
+            var emptynumber = Enumerable.Empty<int>().ToList();
+
+            var repeatnumber = Enumerable.Repeat(0, 100).ToList();
+        }
+    }
+}
